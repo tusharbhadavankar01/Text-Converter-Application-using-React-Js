404788ad3afb9ae655e4859b3343b5ad4ba2aeca
Tushar <bhadavankartushar01@gmail.com>
2024-09-03 13:20:44 +0530

    fixing issues

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -53,20 +53,20 @@ return (
         <div className='container' style={{color:props.mode==="dark" ?"white":"black"}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode==="dark" ?"grey":"white", color:props.mode==="dark" ?"white":"black"}} id="mybox" rows="8"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode==="dark" ?"#676767":"white", color:props.mode==="dark" ?"white":"black"}} id="mybox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-2 my-1 " onClick={handleUpClick}>Convert to UpperCase</button> 
-        <button className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
-        <button className="btn btn-primary mx-2 my-1" onClick={handleCopyClick}>Copy to Clipboard</button>
-        <button className="btn btn-primary mx-2 my-1" onClick={handleRemoveSpace}>Remove Extra Spaces</button>
-        <button className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear Text</button>    
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-1 " onClick={handleUpClick}>Convert to UpperCase</button> 
+        <button  disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
+        <button  disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleCopyClick}>Copy to Clipboard</button>
+        <button  disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleRemoveSpace}>Remove Extra Spaces</button>
+        <button  disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear Text</button>    
     
    
         </div>
         <div className='container my-3' style={{color:props.mode==="dark" ?"white":"black"}}>
         <h2>Your Text Summary</h2>
-        <p>{ text.trim().length === 0 ? 0 : text.split(/\s+/).length} words and {text.length} characters</p>
-        <p>{0.008 * text.split(" ").length} Minutes to read</p>
+        <p>{ text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter something in the above textbox to preview it here"}</p>
         </div>
